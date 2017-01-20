0b372c264d46463013290f2e9c796cee25666956
Raphael Michel <mail@raphaelmichel.de>
2017-01-20 12:48:29 +0100

    Initial cookiecutter commit

---
+++ pretix_sepadebit/__init__.py
@@ -0,0 +1,19 @@
+from django.apps import AppConfig
+
+
+class PluginApp(AppConfig):
+    name = 'pretix_sepadebit'
+    verbose_name = 'SEPA Direct debit for pretix'
+
+    class PretixPluginMeta:
+        name = 'SEPA Direct debit for pretix'
+        author = 'Raphael Michel'
+        description = 'This plugin adds SEPA direct debit support to pretix'
+        visible = True
+        version = '1.0.0'
+
+    def ready(self):
+        from . import signals  # NOQA
+
+
+default_app_config = 'pretix_sepadebit.PluginApp'
---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup, find_packages
+
+
+try:
+    with open(os.path.join(os.path.dirname(__file__), 'README.rst'), encoding='utf-8') as f:
+        long_description = f.read()
+except:
+    long_description = ''
+
+
+setup(
+    name='pretix-sepadebit',
+    version='1.0.0',
+    description='This plugin adds SEPA direct debit support to pretix',
+    long_description=long_description,
+    url='https://pretix.eu/pretix/pretix-sepadebit',
+    author='Raphael Michel',
+    author_email='mail@raphaelmichel.de',
+
+    install_requires=[],
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    include_package_data=True,
+    entry_points="""
+[pretix.plugin]
+pretix_sepadebit=pretix_sepadebit:PretixPluginMeta
+""",
+)
