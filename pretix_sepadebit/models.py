12ef8123c04d58d729415ed0a0ec533423883263
Raphael Michel <mail@raphaelmichel.de>
2018-03-06 16:55:58 +0100

    Prepare for weblate

--- pretix_sepadebit/models.py
+++ pretix_sepadebit/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class SepaExport(models.Model):
-    event = models.ForeignKey('pretixbase.Event', related_name='sepa_exports')
+    event = models.ForeignKey('pretixbase.Event', related_name='sepa_exports', on_delete=models.CASCADE)
     xmldata = models.TextField()
     datetime = models.DateTimeField(auto_now_add=True)
 
 
 class SepaExportOrder(models.Model):
-    export = models.ForeignKey(SepaExport)
-    order = models.ForeignKey('pretixbase.Order')
+    export = models.ForeignKey(SepaExport, on_delete=models.CASCADE)
+    order = models.ForeignKey('pretixbase.Order', on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
