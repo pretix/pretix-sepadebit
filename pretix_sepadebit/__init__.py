f30a3fa1f59d5217fe8535f00551b3a73d3b9d61
Raphael Michel <mail@raphaelmichel.de>
2018-05-03 09:59:23 +0200

    Bump version to 1.3.0

--- pretix_sepadebit/__init__.py
+++ pretix_sepadebit/__init__.py
@@ -11,7 +11,7 @@ class PluginApp(AppConfig):
         author = 'Raphael Michel'
         description = ugettext_lazy('This plugin adds SEPA direct debit support to pretix')
         visible = True
-        version = '1.2.2'
+        version = '1.3.0'
 
     def ready(self):
         from . import signals  # NOQA
--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ cmdclass = {
 
 setup(
     name='pretix-sepadebit',
-    version='1.2.2',
+    version='1.3.0',
     description='This plugin adds SEPA direct debit support to pretix',
     long_description=long_description,
     url='https://github.com/pretix/pretix-sepadebit',
