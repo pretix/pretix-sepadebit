31f31613558e0b62ac453628444f380bcbfb4fd0
Raphael Michel <mail@raphaelmichel.de>
2022-10-27 22:04:52 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ cmdclass = {
 
 setup(
     name='pretix-sepadebit',
-    version='2.0.2',
+    version='2.0.3',
     description='This plugin adds SEPA direct debit support to pretix',
     long_description=long_description,
     url='https://github.com/pretix/pretix-sepadebit',
