79acb0432b446b49858bbc89f5e68cbbcbdda6af
Raphael Michel <michel@pretix.eu>
2025-08-20 13:53:22 +0200

    Migrate to organizer/event-hybrid plugin

--- pretix_sepadebit/apps.py
+++ pretix_sepadebit/apps.py
@@ -1,10 +1,14 @@
-from django.apps import AppConfig
 from django.utils.translation import gettext_lazy
 
 from . import __version__
 
+try:
+    from pretix.base.plugins import PluginConfig, PLUGIN_LEVEL_EVENT_ORGANIZER_HYBRID
+except ImportError:
+    raise RuntimeError("Please use pretix 2025.7 or above to run this plugin!")
 
-class PluginApp(AppConfig):
+
+class PluginApp(PluginConfig):
     name = "pretix_sepadebit"
     verbose_name = "SEPA Direct debit for pretix"
 
@@ -17,7 +21,8 @@ class PluginApp(AppConfig):
         )
         visible = True
         version = __version__
-        compatibility = "pretix>=4.20.0"
+        compatibility = "pretix>=4.18.0.dev0"
+        level = PLUGIN_LEVEL_EVENT_ORGANIZER_HYBRID
 
     def ready(self):
         from . import signals  # NOQA
